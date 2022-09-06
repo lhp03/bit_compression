cfc62898401723015414a72f3f89ce8f020a2a16
kangilkueon <kangilkueon@gmail.com>
2022-09-06 15:21:46 +0900

    Add Skeleton Code

---
+++ pa1.c
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------
+//
+//  4190.308 Computer Architecture (Fall 2022)
+//
+//  Project #1:
+//
+//  September 6, 2022
+//
+//  Seongyeop Jeong (seongyeop.jeong@snu.ac.kr)
+//  Jaehoon Shim (mattjs@snu.ac.kr)
+//  IlKueon Kang (kangilkueon@snu.ac.kr)
+//  Wookje Han (gksdnrwp@snu.ac.kr)
+//  Jinsol Park (jinsolpark@snu.ac.kr)
+//  Systems Software & Architecture Laboratory
+//  Dept. of Computer Science and Engineering
+//  Seoul National University
+//
+//---------------------------------------------------------------
+
+typedef unsigned char u8;
+
+/* TODO: Implement this function */
+int encode(const u8* src, int width, int height, u8* result)
+{
+
+
+
+
+  return 0;
+}
+
