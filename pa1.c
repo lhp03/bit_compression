c3bb19e20d1213d16aa0c130fcd3a2971b7738e8
_pil_ <lhp__@naver.com>
2022-09-18 14:16:25 +0900

    add bit by bit

--- pa1.c
+++ pa1.c
@@ -19,100 +19,127 @@
 
 typedef unsigned char u8;
 
-u8 get_avg(const u8* src, int width, int height, int w, int h) {
-  if (w == 0 && h == 0) {
+u8 get_average(const u8* src, int width, int height, int x, int y) {
+  if (x == 0 && y == 0) {
     return 0;
-  } else if(h != 0 && w == 0) {
-    return *(src + (width * (h - 1)) + w);
-  } else if(h == 0 && w != 0) {
-    return *(src + (width * h + w - 1));
+  } else if(x != 0 && y == 0) {
+    return *(src + (width * y + x - 1));
+  } else if(x == 0 && y != 0) {
+    return *(src + (width * (y - 1)) + x);
   } else {
-    return (*(src + (width * (h - 1)) + w) + *(src + (width * h) + w - 1) + *(src + (width * (h - 1)) + w - 1)) / 3;
+    return (*(src + (width * (y - 1)) + x) + *(src + (width * y) + x - 1) + *(src + (width * (y - 1)) + x - 1)) / 3;
   }
 }
 
-u8 get_filter(const u8* src, int width, int height, int w, int h) {
-  u8 avg = get_avg(src, width, height, w, h);
-  if(*(src + (width * h) + w) >= avg) return *(src + (width * h) + w) - avg;
-  else return *(src + (width * h) + w) + 256 - avg;
+u8 get_filter(const u8* src, int width, int height, int x, int y) {
+  u8 average = get_average(src, width, height, x, y);
+  u8 result;
+
+  if(*(src + (width * y) + x) >= average) {
+    result = *(src + (width * y) + x) - average;
+  } else {
+    result = *(src + (width * y) + x) + 256 - average;
+  }
+
+  return result;
 }
 
-u8 get_min(const u8* src, int width, int height, int h) {
-  u8 min = get_filter(src, width, height, 0, h);
+u8 get_base(const u8* src, int width, int height, int y) {
+  u8 base = get_filter(src, width, height, 0, y);
 
   for(int i = 1; i < width; i++) {
-    if(min > get_filter(src, width, height, i, h)) min = get_filter(src, width, height, i, h);
+    if(base > get_filter(src, width, height, i, y)) {
+      base = get_filter(src, width, height, i, y);
+    }
   }
 
-  return min;
+  return base;
 }
 
-u8 get_max(const u8* src, int width, int height, int h) {
-  u8 max = get_filter(src, width, height, 0, h);
+u8 get_delta(const u8* src, int width, int height, int x, int y) {
+  return get_filter(src, width, height, x, y) - get_base(src, width, height, y);
+}
+
+u8 get_deltaMax(const u8* src, int width, int height, int y) {
+  u8 deltaMax = get_delta(src, width, height, 0, y);
 
   for(int i = 1; i < width; i++) {
-    if(max < get_filter(src, width, height, i, h)) max = get_filter(src, width, height, i, h);
+    if(deltaMax < get_delta(src, width, height, i, y)) {
+      deltaMax = get_delta(src, width, height, i, y);
+    }
   }
 
-  return max;
+  return deltaMax;
 }
 
-u8 get_n(const u8* src, int width, int height, int h) {
-  u8 max = get_max(src, width, height, h) - get_min(src, width, height, h);
-  if(max == 0) return 0;
-  else if(max == 1) return 1;
-  else if(max < 4) return 2;
-  else if(max < 8) return 3;
-  else if(max < 16) return 4;
-  else if(max < 32) return 5;
-  else if(max < 64) return 6;
-  else if(max < 128) return 7;
-  else return 8;
-}
-
-
-u8 get_delta(const u8* src, int width, int height, int w, int h) {
-  u8 base = get_min(src, width, height, h);
-
-  return get_filter(src, width, height, w, h) - base;
-}
-
-void insert_bit(u8* result, u8 value, int len_value, int* bit_index) {
-  if(len_value == 0) return;
-
-  value = value << (8 - len_value);
-  if (*bit_index % 8 == 0) {
-    *(result + *bit_index / 8) = value;
+u8 get_n(const u8* src, int width, int height, int y) {
+  u8 deltaMax = get_deltaMax(src, width, height, y);
+  if(deltaMax == 0) {
+    return 0;
+  } else if(deltaMax == 1) {
+    return 1;
+  } else if(deltaMax < 4) {
+    return 2;
+  } else if(deltaMax < 8) {
+    return 3;
+  } else if(deltaMax < 16){
+    return 4;
+  } else if(deltaMax < 32) {
+    return 5;
+  } else if(deltaMax < 64) {
+    return 6;
+  } else if(deltaMax < 128) {
+    return 7;
   } else {
-    u8 temp = (value >> (*bit_index % 8));
-    value = value << (8 - *bit_index % 8);
-    *(result + *bit_index / 8) = *(result + *bit_index / 8) | temp;
-    *(result + *bit_index / 8 + 1) = value;
+    return 8;
   }
-  *bit_index += len_value;
+}
+
+
+void insert_value_in_result(u8* dst, int* bit_index, u8 value, int bit_len) {
+  if(bit_len == 0 && value == 0) {
+    return;
+  }
+  value = value << (8 - bit_len);
+
+  while(bit_len) {
+    if(*bit_index % 8 == 0) {
+      *(dst + *bit_index / 8) = 0;
+    }
+
+    u8 temp = (value >> 7 << 7 >> (*bit_index % 8));
+
+    *(dst + *(bit_index) / 8) |= temp;
+    *bit_index = *bit_index + 1;
+    value <<= 1;
+    bit_len--;
+  }
 }
 
 /* TODO: Implement this function */
 int encode(const u8* src, int width, int height, u8* result)
 {
-  if (width == 0 || height == 0) return 0;
+  if (width == 0 || height == 0) {
+    return 0;
+  }
 
   int bit_index = 0;
-  for(int i = 0; i < height; i++) {
-    u8 base = get_min(src, width, height, i);
-    u8 n = get_n(src, width, height, i);
 
-    insert_bit(result, base, 8, &bit_index);
-    insert_bit(result, n, 4, &bit_index);
+  for(int i = 0; i < height; i++) {
+    insert_value_in_result(result, &bit_index, get_base(src, width, height, i), 8);
+    insert_value_in_result(result, &bit_index, get_n(src, width, height, i), 4);
 
     for(int j = 0; j < width; j++) {
-      insert_bit(result, get_delta(src, width, height, j, i), (int)n, &bit_index);
+      insert_value_in_result(result, &bit_index, get_delta(src, width, height, j, i), (int)get_n(src,width,height, i));
     }
   }
 
-  int length = bit_index / 8;
-
-  if(bit_index % 8 != 0) length++;
+  int length; 
+  if (bit_index % 8 == 0) {
+    length = bit_index / 8;
+  } else {
+    length = bit_index / 8 + 1;
+  }
 
   return length;
 }
